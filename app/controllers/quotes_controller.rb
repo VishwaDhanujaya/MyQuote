524ad80cb9b331d1b73c56dd87b1b91dcf1a4e59
Vishwa Dhanujaya <58469705+VishwaDhanujaya@users.noreply.github.com>
2025-10-21 00:52:03 +0530

    Allow typed philosopher entry and refine discover layout

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,7 +3,7 @@ class QuotesController < ApplicationController
   before_action :require_active_user
   before_action :set_quote, only: %i[show edit update destroy]
   before_action :authorize_quote_access!, only: %i[show edit update destroy]
-  before_action :load_form_collections, only: %i[new edit create update]
+  before_action :load_categories, only: %i[new edit create update]
 
   def index
     @quotes = if admin?
@@ -20,9 +20,11 @@ class QuotesController < ApplicationController
   end
 
   def create
-    @quote = current_user.quotes.build(quote_params)
+    @quote = current_user.quotes.build
+    attributes = quote_params
+    assign_quote_attributes(@quote, attributes)
 
-    if @quote.save
+    if @quote.errors.empty? && @quote.save
       redirect_to quotes_path, notice: "Quote created successfully."
     else
       render :new, status: :unprocessable_entity
@@ -32,7 +34,10 @@ class QuotesController < ApplicationController
   def edit; end
 
   def update
-    if @quote.update(quote_params)
+    attributes = quote_params
+    assign_quote_attributes(@quote, attributes)
+
+    if @quote.errors.empty? && @quote.save
       redirect_to quotes_path, notice: "Quote updated successfully."
     else
       render :edit, status: :unprocessable_entity
@@ -57,14 +62,61 @@ class QuotesController < ApplicationController
     redirect_to quotes_path, alert: "You are not authorized to access this quote."
   end
 
-  def load_form_collections
-    @philosophers = Philosopher.order(:first_name, :last_name)
+  def load_categories
     @categories = Category.order(:name)
   end
 
   def quote_params
-    permitted = params.require(:quote).permit(:text, :publication_year, :user_comment, :is_public, :philosopher_id, category_ids: [])
-    permitted[:category_ids] = permitted[:category_ids].reject(&:blank?) if permitted[:category_ids].is_a?(Array)
-    permitted
+    permitted = params.require(:quote).permit(
+      :text,
+      :publication_year,
+      :user_comment,
+      :is_public,
+      :philosopher_first_name,
+      :philosopher_last_name,
+      :philosopher_biography,
+      category_ids: []
+    )
+
+    permitted[:category_ids] = Array(permitted[:category_ids]).reject(&:blank?)
+    permitted.to_h.symbolize_keys
+  end
+
+  def assign_quote_attributes(quote, attributes)
+    attrs = attributes.dup
+    category_ids = attrs.delete(:category_ids)
+    philosopher_attrs = attrs.extract!(:philosopher_first_name, :philosopher_last_name, :philosopher_biography)
+
+    quote.assign_attributes(attrs)
+    quote.category_ids = category_ids if category_ids
+
+    assign_philosopher(quote, philosopher_attrs)
+  end
+
+  def assign_philosopher(quote, philosopher_attrs)
+    first_name = philosopher_attrs[:philosopher_first_name].to_s.strip
+    last_name = philosopher_attrs[:philosopher_last_name].to_s.strip
+    biography = philosopher_attrs[:philosopher_biography].to_s.strip
+
+    if first_name.blank?
+      quote.errors.add(:philosopher_first_name, "can't be blank")
+      return false
+    end
+
+    philosopher = Philosopher
+                  .where("LOWER(first_name) = ? AND LOWER(COALESCE(last_name, '')) = ?", first_name.downcase, last_name.downcase)
+                  .first_or_initialize
+
+    philosopher.first_name = first_name
+    philosopher.last_name = last_name.presence
+    philosopher.biography = biography.presence
+
+    unless philosopher.save
+      philosopher.errors.full_messages.each { |message| quote.errors.add(:philosopher, message) }
+      return false
+    end
+
+    quote.philosopher = philosopher
+    true
   end
 end
